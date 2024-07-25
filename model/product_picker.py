ad7dc58196905da0775c5046d16eeff487f6ccd6
Matyáš Martan <martama1@fel.cvut.cz>
2024-07-25 17:29:49 +0200

    minor typo fixes

--- model/product_picker.py
+++ model/product_picker.py
@@ -1,37 +1,5 @@
 import json
-from typing import Dict, Any
-
-
-class ProductProperties:
-    """
-    Class that holds the properties of a product
-    """
-
-    def __init__(self, category, gender, age, customer_disclaimer: str, image: str):
-        self.category = category
-        self.gender = gender
-        self.age = age
-        self.disclaimer = customer_disclaimer
-        self.image = image
-
-    def __str__(self):
-        return (f"Product falls under category {self.category}, "
-                f"is supposed to be used by the gender {self.gender}, "
-                f"of age {self.age}")
-
-
-class Product:
-    """
-    Class that represents the product entity, separated from properties for future GUI implementation
-    """
-
-    def __init__(self, name, properties: ProductProperties):
-        self.name = name
-        self.properties = properties
-
-    def __str__(self):
-        return f"{self.name}\n\t{self.properties}"
-
+from product import ProductProperties, Product
 
 def deserialize_products(filename: str) -> list:
     """
@@ -45,10 +13,10 @@ def deserialize_products(filename: str) -> list:
         products_data = json.load(file)
         for product_data in products_data:
             properties = ProductProperties(category=product_data["properties"]["category"],
-                                           gender=product_data["properties"]["gender"],
-                                           age=product_data["properties"]["age"],
-                                           customer_disclaimer=product_data["properties"]["customer_disclaimer"],
-                                           image=product_data["properties"]["image"])
+                                                   gender=product_data["properties"]["gender"],
+                                                   age=product_data["properties"]["age"],
+                                                   customer_disclaimer=product_data["properties"]["customer_disclaimer"],
+                                                   image=product_data["properties"]["image"])
             product = Product(name=product_data["name"], properties=properties)
             products.append(product)
     return products
@@ -93,11 +61,3 @@ def serialize_products(products: list, filename: str) -> None:
             print(f"Error while serializing products: {e}")
 
 
-# test
-if __name__ == "__main__":
-
-    products = deserialize_products("../tests/test_files/products_sample.json")
-    evaluated_products = pick_products(3, products)
-    for item in evaluated_products:
-        print(item)
-    serialize_products(evaluated_products, "../tests/test_files/picked_products_sample.json")
