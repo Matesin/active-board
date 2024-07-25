cb12c77543137392dcd3b852f100f9f1b43be3b0
Matyáš Martan <martama1@fel.cvut.cz>
2024-07-25 11:12:27 +0200

    improved serialization functionalities

--- product_picker.py
+++ product_picker.py
@@ -7,14 +7,16 @@ class ProductProperties:
     Class that holds the properties of a product
     """
 
-    def __init__(self, category, gender, age):
+    def __init__(self, category, gender, age, customer_disclaimer: str, image: str):
         self.category = category
         self.gender = gender
         self.age = age
+        self.disclaimer = customer_disclaimer
+        self.image = image
 
     def __str__(self):
         return (f"Product falls under category: {self.category}, "
-                f"is supposed to be used by the gender {self.gender},"
+                f"is supposed to be used by the gender {self.gender}, "
                 f"of age {self.age}")
 
 
@@ -44,29 +46,58 @@ def deserialize_products(filename: str) -> list:
         for product_data in products_data:
             properties = ProductProperties(category=product_data["properties"]["category"],
                                            gender=product_data["properties"]["gender"],
-                                           age=product_data["properties"]["age"])
+                                           age=product_data["properties"]["age"],
+                                           customer_disclaimer=product_data["properties"]["customer_disclaimer"],
+                                           image=product_data["properties"]["image"])
             product = Product(name=product_data["name"], properties=properties)
             products.append(product)
     return products
 
 
-def pick_products(products_threshold: int, products) -> dict[Any, int]:
+def pick_products(products_threshold: int, products) -> list:
     """
     Picks products based on given parameters
     :param products_threshold: Number of products to pick
     :param products: List of products
     :return: Given number of products that are the most attractive
     """
-    evaluated_products = {} # Dictionary of picked products and their attractiveness
+    evaluated_products = {}  # Dictionary of picked products and their attractiveness
     for product in products:
         attractiveness = 1
         # Add some heuristics here
         evaluated_products[product] = attractiveness
-    evaluated_products.sort(key=lambda x: x[1], reverse=True)
-    return evaluated_products[:products_threshold]
+    # Sort the products by their attractiveness
+    evaluated_products_list = [product for product, _ in
+                               sorted(evaluated_products.items(), key=lambda item: item[1], reverse=True)]
+    return evaluated_products_list[:products_threshold]
+
+
+def serialize_products(products: list, filename: str) -> None:
+    """
+    Serializes products to a given file
+    :param products: List of products to serialize
+    :param filename: Name of the file to serialize
+    :return: None
+    """
+    products_data = []
+    for product in products:
+        product_data = {"name": product.name,
+                        "properties":
+                            {"category": product.properties.category,
+                             "customer_disclaimer": product.properties.disclaimer,
+                             "image": product.properties.image}}
+    with open(filename, "w") as file:
+        try:  # Try to serialize the products
+            json.dump(products_data, file)
+        except json.JSONDecodeError as e:
+            print(f"Error while serializing products: {e}")
+
 
 # test
 if __name__ == "__main__":
-    products = pick_products(5, "products_sample.json")
-    for product in products:
-        print(product)
+
+    products = deserialize_products("products_sample.json")
+    evaluated_products = pick_products(3, products)
+    for item in evaluated_products:
+        print(item)
+    serialize_products(evaluated_products, "picked_products_sample.json")
